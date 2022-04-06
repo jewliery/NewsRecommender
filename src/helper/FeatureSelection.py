cdd6117548ec7e20711448dd70dd8e8710fc194d
Jules <julienne.buechele@gmail.com>
2022-04-06 10:10:24 -0400

    Klassenhierarchie init

---
+++ src/helper/FeatureSelection.py
@@ -0,0 +1,7 @@
+class FeatureSelection():
+
+    def getKeywords(self, tweet):
+        return
+
+    def generateVector(self, tweet):
+        return
---
+++ src/helper/Learner.py
@@ -0,0 +1,7 @@
+class Learner():
+
+    def fit(self):
+        return
+
+    def predict(self):
+        return
